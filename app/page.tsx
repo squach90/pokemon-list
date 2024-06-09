b582d71c502cf9cc0f211f90cdfb2f37f69021c4
squach90 <160175686+squach90@users.noreply.github.com>
2024-06-09 20:02:01 +0200

    Test fix 1

--- app/page.tsx
+++ app/page.tsx
@@ -1,26 +1,19 @@
-"use client";
-
-import React, { useState, useEffect } from "react";
+// page.tsx
+import React from "react";
 import PokemonCard from "./components/PokemonCard";
 
 interface Pokemon {
   name: string;
   type: string;
   description: string;
+  weight: string;
+  height: string;
 }
 
-const Home: React.FC = () => {
-  const [pokemons, setPokemons] = useState<Pokemon[]>([]);
-
-  useEffect(() => {
-    fetch("/api/pokemons")
-      .then((response) => response.json())
-      .then((data) => setPokemons(data));
-  }, []);
-
+const Page: React.FC<{ pokemons: Pokemon[] }> = ({ pokemons }) => {
   return (
     <div className="container mx-auto p-4">
-      <h1 className="text-3xl font-bold mb-4">Liste des Pokémon</h1>
+      <h1 className="text-3xl font-bold mb-4">Liste des Pokémons</h1>
       <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
         {pokemons.map((pokemon, index) => (
           <PokemonCard key={index} pokemon={pokemon} />
@@ -30,4 +23,4 @@ const Home: React.FC = () => {
   );
 };
 
-export default Home;
+export default Page;
